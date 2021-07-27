886ddcdd19cb8773d41d22bf5b6ecd7c5b466f62
adithyasankar47 <adithyasankar47@gmail.com>
2021-07-27 23:16:06 +0530

    Updated gitignore to stop addition of exe files

--- STL_examples/stl_array.cpp
+++ STL_examples/stl_array.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int main()
 {
-    array <int,4> a = {1,2,3,4};
+    array<int, 4> a = {1, 2, 3, 4};
     int size = a.size();
 
-    for(int i=0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
-        cout<<a[i]<<" ";
+        cout << a[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 
-    cout<<"Element at 2nd Index is -> "<<a.at(2)<<endl;
-    cout<<"Empty or not -> "<<a.empty()<<endl;
-    cout<<"First element -> "<<a.front()<<endl;
-    cout<<"Last elemetn -> "<<a.back()<<endl;
+    cout << "Element at 2nd Index is -> " << a.at(2) << endl;
+    cout << "Empty or not -> " << a.empty() << endl;
+    cout << "First element -> " << a.front() << endl;
+    cout << "Last elemetn -> " << a.back() << endl;
     return 0;
 }
